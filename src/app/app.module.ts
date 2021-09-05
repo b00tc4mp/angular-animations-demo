c6083cb9d111f996a935e56ef5c3e90a177d51bf
manuelbarzi <manuelbarzi@gmail.com>
2021-09-05 17:50:02 +0200

    initial import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppComponent } from './app.component';
+import { OpenCloseComponent } from './open-close/open-close.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OpenCloseComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
